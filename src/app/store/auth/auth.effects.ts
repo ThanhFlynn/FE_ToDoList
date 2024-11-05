72dafbd3255dfd643984b2426a03c7cd554b24b3
ThanhFlynn <thanhnct244@gmail.com>
2024-11-05 22:47:50 +0700

    reset proj

--- src/app/store/auth/auth.effects.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@angular/core';
-import { Actions, createEffect, ofType } from '@ngrx/effects';
-import * as AuthActions from './auth.actions';
-
-@Injectable()
-export class AuthEffects {
-  constructor(private actions$: Actions) {}
-}
